53330a7b410d10c285f2be7d3a69f5502509038a
Kevin Grimaldi <anthgrim@icloud.com>
2023-01-23 15:21:18 -0500

    adding insertion sort

---
+++ Algorithms/Sorting/InsertionSort.js
@@ -0,0 +1,27 @@
+// Not the most efficient
+// There are cases that is extremely fast
+// Use, when you are sure the list is almost sorted
+// For small datasets
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+/**
+ * @description Insertion sort
+ * @param {Array} array
+ */
+const insertionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0])
+    } else {
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          array.splice(j, 0, array.splice(i, 1)[0])
+        }
+      }
+    }
+  }
+}
+
+insertionSort(numbers)
+console.log(numbers)
